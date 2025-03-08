738c0ddf03a2dee065c33c6c054e9d2397eaab96
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-08 15:44:02 +0200

    Add related_name for user field in Reservation model & run migrations

--- theatre/models.py
+++ theatre/models.py
@@ -95,6 +95,7 @@ class Reservation(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
+        related_name="reservations",
     )
 
     class Meta:
