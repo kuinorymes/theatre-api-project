87e85d2445b0cf3b7659ca3d39e341c6dc5a8fbf
Arsen Myroniuk <arisenmyr@gmail.com>
2025-03-16 19:36:02 +0200

    -Changed path for user endpoints: "api/user/" > "api/users/"

--- theatre_service/urls.py
+++ theatre_service/urls.py
@@ -12,7 +12,7 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/theatre/", include("theatre.urls", namespace="theatre")),
-    path("api/user/", include("user.urls", namespace="user")),
+    path("api/users/", include("user.urls", namespace="user")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger/",
